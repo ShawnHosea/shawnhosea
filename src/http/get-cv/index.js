76ec5ea2155050cb64c457608fd93c759185eb91
ShawnHosea <shawnhosea@shawnhosea.com>
2022-07-03 15:48:25 -0400

    update

--- src/http/get-cv/index.js
+++ src/http/get-cv/index.js
@@ -25,6 +25,7 @@ exports.handler = arc.http.async(index)
   </div>
 
   <hr>
+
   <div class="technical">
     <h3>Technical</h3>
     <ul class="techs">
@@ -41,6 +42,7 @@ exports.handler = arc.http.async(index)
   
   <div class="experience">
   <h3>Experience</h3>
+
     <div>
       <div class="exp">
         <strong>Begin | </strong><span>Junior Engineer</span> 
@@ -50,47 +52,69 @@ exports.handler = arc.http.async(index)
         <p>Automated manual report generation for legacy ticketing system to provide reliable team analytics.</p>
         <p>Provided quality solutions to mission critical production issues in a timely manner.</p>
         <p>Proposed, designed and implemented a realtime tracking, deployment and notification system for test environments.</p>
-
+        <br>
         <ul class="tasks">
-        <li>drive</li>
-        <li>drive</li>
-        <li>drive</li>
-        <li>drive</li>
+          <li>drive</li>
+          <li>drive</li>
+          <li>drive</li>
+          <li>drive</li>
         </ul>
-        <strong>Technical Environment: </strong>OpenJS Architecht, Node, AWS.</strong>
+        <br>
+        <blockquote><strong>Technical Environment: </strong>OpenJS Architect, Node, AWS.</blockquote>
       </div>
    
-      <div>
+      <div class="exp">
         <strong>Fortamus | </strong><span>UI Developer</span> 
         <p>2019 to 2019</p>
-        Designed and implemented features and improvements for multiple enterprise sized applications.<br>    Automated manual report generation for legacy ticketing system to provide reliable team analytics.<br>    Provided quality solutions to mission critical production issues in a timely manner.<br>    Proposed, designed and implemented a realtime tracking, deployment and notification system for test environments.<br>    
-        <strong>Technical Environment: </strong>React, Ant Design.
+        <br>
+        <p>Designed and implemented features and improvements for multiple enterprise sized applications.</p>
+        <p>Automated manual report generation for legacy ticketing system to provide reliable team analytics.</p>
+        <p>Provided quality solutions to mission critical production issues in a timely manner.</p>
+        <p>Proposed, designed and implemented a realtime tracking, deployment and notification system for test environments.</p>
+        <br>
+        <ul class="tasks">
+          <li>drive</li>
+          <li>drive</li>
+          <li>drive</li>
+          <li>drive</li>
+        </ul>
+        <br>    
+        <blockquote><strong>Technical Environment: </strong>React, Ant Design.</blockquote>
       </div>
 
-      <div>
-        <strong>Swizec | </strong> <span>Technical Consultant</span> <p>2011</p>
-        <br>Architected and implemented a interactive media streaming service that enabled realtime, massively multiplayer, jeopardy-style interactions.<br>    Built host interface to inject synchronized prompts, overlays and widgets into live video stream.<br>    Created detailed documentation regarding technical requirements, project time lines and staffing guidelines for realizing total product offering.
-        <strong>Technical Environment: </strong> React, Gatsby, Javascript.
+      <div class="exp">
+        <strong>Swizec | </strong><span>Apprentice</span> 
+        <p>2018 to 2019</p>
+        <p>Architected and implemented a interactive media streaming service that enabled realtime, massively multiplayer, jeopardy-style interactions.</p>
+        <p>Built host interface to inject synchronized prompts, overlays and widgets into live video stream.</p>
+        <p>Created detailed documentation regarding technical requirements, project time lines and staffing guidelines for realizing total product offering.</p>
+        <br>
+        <ul class="tasks">
+          <li>drive</li>
+          <li>drive</li>
+          <li>drive</li>
+          <li>drive</li>
+        </ul>
+        <br>
+        <blockquote><strong>Technical Environment: </strong> React, Gatsby, Javascript.</blockquote>
       </div>
     </div>
   </div>
 
   <hr>
 
-  <h3 id="projects">Projects</h3>
-  <ul>
-    <li><p><strong>Patromate</strong><br>  <a href=http://www.patromate.com class=not-printed>patromate.com</a><br>  Cordova based app(iOS &amp; Android) that leverages a huge amalgamation of public data. Driven by over 30 web spiders, 15 webservices and multiple external databases it provides brand ratings from non-profit organisations by scanning product barcodes.</p>
-    </li>
-    <li><p><strong>Halifax Crime Heatmap</strong><br>  <a href=http://www.crimeheatmap.ca class=not-printed>crimeheatmap.ca</a><br>  HTML5 Animated heatmap visualising crimes in the city of Halifax. Built on a customised version Heatmap.js library which enables smooth animation. Won silver in the <a href="http://apps4halifax.ca/">Apps4Halifax</a> city category.</p>
-    </li>
-    <li><p><strong>Foxish</strong><br>  <a href=https://chrome.google.com/webstore/detail/jpgagcapnkccceppgljfpoadahaopjdb class=not-printed>chrome.google.com</a><br>  Chrome extension that enables Firefox-style live RSS feeds with over fifteen thousand active users a 4/5 star rating. Featured on <a href="https://gizmodo.com/5609633/10-add+ons-you-have-to-know-about-for-google-chrome">Gizmodo</a> &amp; <a href="https://lifehacker.com/5603602/foxish-live-rss-adds-live-bookmarks-to-google-chrome">LifeHacker</a>.</p>
-    </li>
-  </ul>
+  <div class="projects">
+    <h3>Projects</h3>
+    <ul class="projectGroup">
+      <li><p><strong>Patromate</strong><br>  <a href=http://www.patromate.com>patromate.com</a><br>  Cordova based app(iOS &amp; Android) that leverages a huge amalgamation of public data. Driven by over 30 web spiders, 15 webservices and multiple external databases it provides brand ratings from non-profit organisations by scanning product barcodes.</p>
+      </li>
+      <li><p><strong>PushThemes</strong><br>  <a href=http://www.pushthemes.com>pushthemes.com</a><br>  HTML5 Animated heatmap visualising crimes in the city of Halifax. Built on a customised version Heatmap.js library which enables smooth animation. Won silver in the <a href="http://apps4halifax.ca/">Apps4Halifax</a> city category.</p>
+      </li>
+      <li><p><strong>Noticate</strong><br>  <a href=https://noticate.com>noticate.com</a><br>  Chrome extension that enables Firefox-style live RSS feeds with over fifteen thousand active users a 4/5 star rating.</p>
+      </li>
+    </ul>
+  </div>
 
-  <hr>
-
-  <h3 id="education">Education</h3>
-  <p><strong>Bachelor of Technology (Software Development)</strong> <strong>2003 to 2007</strong><br>    Seneca College of Applied Arts and Technology (<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="0a596f646f696b4a53657861">[email&#160;protected]</a>), Toronto, Ontario</p>
 </div>
     `
   })
