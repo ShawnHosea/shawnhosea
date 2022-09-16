cc18dcf3e9355a1ed71c81364ecd63ba92f2ed3a
ShawnHosea <shawnhosea@shawnhosea.com>
2022-09-16 14:48:54 -0400

    Checkpoint

--- src/http/get-cv/index.js
+++ src/http/get-cv/index.js
@@ -18,7 +18,7 @@ exports.handler = arc.http.async(index)
       <h2>Full Stack Engineer &amp; Product Enthusiast</h2>
     </div>
     <div class="contact">
-      <p><a href="https://www.shawnhosea.com">shawnhosea.com</a>
+    <p><a href="https://github.com/ShawnHosea">github.com/ShawnHosea</a></p>
       <p>shawnhosea@shawnhosea.com</p>
       <p>(404) 610-0305</p>
     </div>
@@ -30,8 +30,8 @@ exports.handler = arc.http.async(index)
     <h3>Technical</h3>
     <ul class="techs">
       <li>UI design | HTML/CSS</li>
-      <li>Javascript / NodeJS</li>
-      <li>Linux / Bash</li>
+      <li>Javascript/NodeJS</li>
+      <li>Linux/Bash</li>
       <li>AWS | cloud/serverless</li>
       <li>APIs/REST/OAuth</li>
       <li>Git</li>
@@ -47,17 +47,17 @@ exports.handler = arc.http.async(index)
 
     <div>
       <div class="exp">
-        <strong>Begin | </strong><span>FullStack Engineer</span> 
+        <strong>Begin.com | </strong><span>FullStack Engineer</span> 
         <p>2020 to 2022</p>
         <br>    
         <p>Worked on day-to-day frontend/backend maintenance of company product alongside founders.</p>
         <p>Prototyped new features and pushed several updates to production.</p>
         <br>
         <ul class="tasks">
-          <li>Built company blog site.</li>
+          <li>Built company blog site. <a href="https://blog.begin.com/">https://blog.begin.com/</a></li>
           <li>Wrote product documentation and technical articles.</li>
           <li>Created technical video explainers showcasing product features.</li>
-          <li>Shipped weekly PRs to the main product (Begin) and open-source community (Architect).</li>
+          <li>Shipped weekly PRs to the main product (Begin.com) and open-source community (arc.codes).</li>
           <li>Regularly kept repositories and changelogs updated.</li>
           <li>Helped troubleshoot customer issues with product.</li>
         </ul>
@@ -83,14 +83,15 @@ exports.handler = arc.http.async(index)
       </div>
 
       <div class="exp">
-        <strong>Swizec Inc. | </strong><span>Apprentice</span> 
+        <strong>Swizec Inc. | </strong><span>Junior Developer</span> 
         <p>2018 to 2019</p>
-        <p>Apprenticeship position where I was tasked with building marketing sites for several digital products using React & Gatsby.</p>
+        <p>Junior developer position where I was tasked with building marketing sites for several digital products using React & Gatsby.</p>
         <br>
         <ul class="tasks">
           <li>Created reusable React components for landing pages.</li>
           <li>Designed overall product branding and user experience.</li>
           <li>Self-initiated coding of prototypes for user testing to aid in other designers projects.</li>
+          <li>Created <a href="https://d3blackbox.com/">https://d3blackbox.com/</a></li>
         </ul>
         <br>
         <blockquote><strong>Technical Environment: </strong> React, Gatsby, Javascript.</blockquote>
@@ -108,14 +109,14 @@ exports.handler = arc.http.async(index)
         <br>
         <a href=https://patromate.com>patromate.com</a>
         <br>  
-        <p>Utilizing OAuth2 provided by the Patreon API, this app allows users to download a .csv file containing a list of all posts in thier Patreon account.</p>
+        <p>Utilizing OAuth2 provided by the Patreon API, this app allows users to download a .csv file containing a list of all posts in their Patreon account.</p>
       </li>
       <li>
         <strong>PushThemes</strong>
         <br>  
         <a href=https://pushthemes.com>pushthemes.com</a>
         <br>  
-        <p>This site and the accompanying subdomains is a repository of themes for Ghost users to create thier own blogs with. It utilizes the Ghost API to allow users to create unique designs with the theme. The site is built and hosted on an AWS EC2 instance.</p>
+        <p>This site and the accompanying subdomains are a repository of themes for Ghost users to create blogs. It utilizes the Ghost API to allow users to create unique designs with the theme. The site is built and hosted on an AWS EC2 instance.</p>
       </li>
       <li>
         <strong>Noticate</strong>
